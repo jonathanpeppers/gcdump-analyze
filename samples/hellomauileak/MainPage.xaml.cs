6e53cb996bb6383e6dfd499bd03d1adea331cc28
Jonathan Peppers <jonathan.peppers@microsoft.com>
2025-08-19 14:07:20 -0500

    LeakyPage example

--- samples/hellomauileak/MainPage.xaml.cs
+++ samples/hellomauileak/MainPage.xaml.cs
@@ -7,6 +7,15 @@ public partial class MainPage : ContentPage
         InitializeComponent();
     }
 
+    protected override void OnNavigatedTo(NavigatedToEventArgs args)
+    {
+        base.OnNavigatedTo(args);
+
+        // Run some GCs, to make .gcdump have less objects
+        for (int i = 0; i < 3; i++)
+            GC.Collect();
+    }
+
     private async void OnCounterClicked(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new LeakyPage());
